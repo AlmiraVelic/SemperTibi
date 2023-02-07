e8765fd670a7f1809a1c0f7f97b9a235b252b1ef
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-02-07 12:23:24 +0100

    Youtube video added to tippBreath.kt and xml

--- app/src/main/java/com/example/sempertibi/tippBreath.kt
+++ app/src/main/java/com/example/sempertibi/tippBreath.kt
@@ -5,20 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class tippBreath : AppCompatActivity() {
     private lateinit var playerView: YouTubePlayerView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var titleTv: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tipp_breath)
 
         // https://github.com/PierfrancescoSoffritti/android-youtube-player
         // Library to embed Youtube videos
-
         playerView = findViewById(R.id.VideoBreath)
         lifecycle.addObserver(playerView)
     }
