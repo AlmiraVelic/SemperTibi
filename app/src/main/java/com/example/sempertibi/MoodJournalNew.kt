c1463057e3faaa1b76c20290407147ebdc447c74
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-02-27 16:35:48 +0100

    automatic date and date format set; input text added to database

--- app/src/main/java/com/example/sempertibi/MoodJournalNew.kt
+++ app/src/main/java/com/example/sempertibi/MoodJournalNew.kt
@@ -10,29 +10,48 @@ import com.example.sempertibi.data.UserDatabase
 import com.example.sempertibi.data.entities.MoodJournal
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 class MoodJournalNew : AppCompatActivity() {
+
+    lateinit var moodSituationInput: TextInputEditText
+    lateinit var moodEmotionInput: TextInputEditText
+    lateinit var moodAchievementInput: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mood_journal_new)
 
         val dao = UserDatabase.getInstance(this).userDao()
 
-        val moodSituation = findViewById<TextInputEditText>(R.id.tvSituationInput).text.toString()
-        val moodEmotion = findViewById<TextInputEditText>(R.id.tvEmotionInput).text.toString()
-        val moodAchievement = findViewById<TextInputEditText>(R.id.tvAchievementInput).text.toString()
+        // Date of the entry
+        val currentDate = SimpleDateFormat("dd-MM-yyy").format(Date())
+        // Entry fields
 
-        val insertMood = listOf(
-            MoodJournal(0,1, "01.01.2023", moodSituation, moodEmotion, moodAchievement, 1 )
-        )
+        moodSituationInput = findViewById(R.id.tvSituationInput)
+        moodEmotionInput = findViewById(R.id.tvEmotionInput)
+        moodAchievementInput = findViewById(R.id.tvAchievementInput)
+
 
         val newEntry = findViewById<Button>(R.id.btnSaveEntry)
-        newEntry.setOnClickListener{
-            lifecycleScope.launch{
+        newEntry.setOnClickListener {
+            val moodSituation = moodSituationInput.text.toString()
+            val moodEmotion = moodEmotionInput.text.toString()
+            val moodAchievement = moodAchievementInput.text.toString()
+
+            val insertMood = listOf(
+                MoodJournal(0, 1, currentDate, moodSituation, moodEmotion, moodAchievement, 1)
+            )
+
+            lifecycleScope.launch {
                 insertMood.forEach { dao.addMood(it) }
             }
             Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show()
+
         }
 
         val icon = findViewById<ImageView>(R.id.logo)
