8363488762f276390fbc7ef258d78210343d08ab
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-03-12 15:48:47 +0100

    SQL Statements added for all entities

--- app/src/main/java/com/example/sempertibi/data/UserDao.kt
+++ app/src/main/java/com/example/sempertibi/data/UserDao.kt
@@ -6,8 +6,6 @@ import com.example.sempertibi.data.entities.MoodJournal
 import com.example.sempertibi.data.entities.StressHRV
 import com.example.sempertibi.data.entities.StressPSS
 import com.example.sempertibi.data.entities.User
-import com.example.sempertibi.data.relations.UserWithMoodjournal
-import java.util.Date
 
 /*
 Dao = Data Access Object
@@ -17,26 +15,60 @@ It contains the methods used for accessing the database
 @Dao
 interface UserDao {
     // If there is same user we would ignore that now
+
+    // User database interactions
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addUser(user: User)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addMood(moodJournal: MoodJournal)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addStressPSS(testPSS: StressPSS)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun addStressHRV(testHRV: StressHRV)
 
     @Query("SELECT * FROM user ORDER BY user_id ASC")
     fun readAllUserData(): LiveData<List<User>>
 
-    // Thread safe manner
-    @Transaction
-    // Query the situation, that is inserted in the Moodjournal for a specific day
-    @Query(value = "SELECT * FROM moodJournal WHERE user_id = :user_id AND entry_date = :entry_date")
-    fun readMoodOfUserData(user_id: Int, entry_date: String): List<MoodJournal>
+    @Update
+    suspend fun updateUser(user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
+
+    // Mood Journal database interactions
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addMood(moodJournal: MoodJournal)
+
+    @Transaction    // Thread safe manner
+    @Query("SELECT * FROM moodJournal WHERE user_id = :user_id AND entry_date = :entry_date")// Query the situation, that is inserted in the Moodjournal for a specific day
+    fun readMoodOfUser(user_id: Int, entry_date: String): List<MoodJournal>
+
+    @Update
+    suspend fun updateMood(user: User)
+
+    @Delete
+    suspend fun deleteMood(user: User)
+
+    // Stress Test PSS database interactions
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addStressPSS(testPSS: StressPSS)
+
+    @Query("SELECT * FROM stressPSS ORDER BY user_id ASC")
+    fun readStressTestPSS(): LiveData<List<StressPSS>>
+
+    @Update
+    suspend fun updateStressTestPSS(testPSS: StressPSS)
+
+    @Delete
+    suspend fun deleteStressTestPSS(testPSS: StressPSS)
+
+    // Stress Test HRV database interactions
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addStressHRV(testHRV: StressHRV)
+
+    @Query("SELECT * FROM stressHRV ORDER BY user_id ASC")
+    fun readStressTestHRV(): LiveData<List<StressHRV>>
+
+    @Update
+    suspend fun updateStressTestHRV(testHRV: StressHRV)
+
+    @Delete
+    suspend fun deleteStressTestHRV(testHRV: StressHRV)
+
 
     @Query("SELECT EXISTS(SELECT * FROM moodJournal Where user_id = :user_id AND entry_date = :entry_date)")
     fun isRowIsExists(user_id: Int, entry_date: String): Boolean
