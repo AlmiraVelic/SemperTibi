5d5307070959e857c6819f02503023ae9b68d17a
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-03-13 18:56:12 +0100

    BCrypt function implemented to check login data with registered data. Logs for debugging added. Direction of intent changed to Dashboard activity

--- app/src/main/java/com/example/sempertibi/SigninActivity.kt
+++ app/src/main/java/com/example/sempertibi/SigninActivity.kt
@@ -2,17 +2,26 @@ package com.example.sempertibi
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.sempertibi.data.UserDatabase
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.mindrot.jbcrypt.BCrypt
 
 class SigninActivity : AppCompatActivity() {
 
-    lateinit var userInputField : TextInputEditText
-    lateinit var passwordInputField : TextInputEditText
-    lateinit var relativeLayoutSignIn : RelativeLayout
+    lateinit var userInputField: TextInputEditText
+    lateinit var passwordInputField: TextInputEditText
+    lateinit var relativeLayoutSignIn: RelativeLayout
+    lateinit var loginbtn: Button
+    lateinit var registerbtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,37 +30,61 @@ class SigninActivity : AppCompatActivity() {
         userInputField = findViewById(R.id.usernameInput)
         passwordInputField = findViewById(R.id.passwordInput)
         relativeLayoutSignIn = findViewById(R.id.relativeLayout_signin)
+        loginbtn = findViewById(R.id.btLogin)
+        registerbtn = findViewById(R.id.btRegisterSignIn)
 
-        var user: String
-        val login = findViewById<Button>(R.id.btLogin)
+        val userDao = UserDatabase.getInstance(this).userDao()
+        loginbtn.setOnClickListener {
 
-        login.setOnClickListener{
-            user = userInputField.text.toString()
-            if (validateInput()){
-                showMessage("Login Successful")
-                val intent = Intent(this, SecondActivity::class.java)
-                intent.putExtra("USER", user)
-                startActivity(intent)
+            val user = userInputField.text.toString().trim()
+            val password = passwordInputField.text.toString().trim()
+
+            lifecycleScope.launch {
+                Log.d("CoroutineDebug", "Coroutine started")
+                val userInDB = withContext(Dispatchers.IO) {
+                    Log.d("CoroutineDebug", "Coroutine suspended: retrieving user")
+                    userDao.getUserByUsername(user)
+                }
+                Log.d("CoroutineDebug", "Coroutine resumed: user retrieved")
+
+                if (validateInput()) {
+                    //Verify the password against the stored hash using secure hashing algorithm BCrypt
+                    if (userInDB != null && BCrypt.checkpw(password,userInDB.passwordHash)) {
+                        Log.d("CoroutineDebug", "Authentication succeeded")
+                        // Authentication succeeded
+                        showMessage("Login Successful")
+                        withContext(Dispatchers.Main) {
+                            val intent = Intent(this@SigninActivity, Dashboard::class.java)
+                            intent.putExtra("USER", user)
+                            startActivity(intent)
+                            finish()
+                        }
+                    } else {
+                        Log.d("CoroutineDebug", "Authentication failed")
+                        // Authentication failed
+                        showMessage("Login failed")
+                    }
+                }
             }
         }
 
-        val register = findViewById<Button>(R.id.btRegisterSignIn)
-        register.setOnClickListener{
+        registerbtn.setOnClickListener {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
     }
+
     // Shows a message in the bottom line of the Screen
-    private fun showMessage(message: String){
+    private fun showMessage(message: String) {
         Snackbar.make(relativeLayoutSignIn, message, Snackbar.LENGTH_SHORT).show()
     }
 
     // Validates the input of User and Password
-    private fun validateInput():Boolean{
-        if (userInputField.text.toString().trim().isEmpty()){
+    private fun validateInput(): Boolean {
+        if (userInputField.text.toString().trim().isEmpty()) {
             showMessage("Please enter Username")
             return false
-        } else if (passwordInputField.text.toString().trim().isEmpty()){
+        } else if (passwordInputField.text.toString().trim().isEmpty()) {
             showMessage("Please enter Password")
             return false
         }
