82b43a0c440af548306103be42a91e5a0c6ca0fb
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-03-14 19:04:01 +0100

    Global data for reuse in app activities

---
+++ app/src/main/java/com/example/sempertibi/GlobalData.kt
@@ -0,0 +1,14 @@
+package com.example.sempertibi
+
+object GlobalData {
+    // Login and user data for settings
+    var userID: Int? = null
+    var loggedInUser: String? = null
+    var passwordUser: String? = null
+    var emailUser: String? = null
+    var notificationUser: Boolean? = null
+    var genderUser: String? = null
+
+    /* TODO Stress Test global data */
+
+}
