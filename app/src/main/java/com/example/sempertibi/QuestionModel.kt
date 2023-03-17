71fad78b7951723fc4e5ba9ad997df7af5d66c7a
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-03-17 17:02:35 +0100

    deleted because the Room is running

--- app/src/main/java/com/example/sempertibi/QuestionModel.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sempertibi
-
-class QuestionModel(
-    val id: String, val question: String,
-    val option1: String, val option2: String, val option3: String, val option4: String, val option5: String, val answerNr: String)
--- app/src/main/java/com/example/sempertibi/UserModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.sempertibi
-
-class UserModel (val id: Int = -1, val name: String, val password: String, val gender: String, val email: String)
