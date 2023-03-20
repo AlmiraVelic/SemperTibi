72d595ae64ab2e47207dac360fb48aaa93bf0c03
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-03-20 12:14:42 +0100

    automated Sign in activity start

--- app/src/main/java/com/example/sempertibi/MainActivity.kt
+++ app/src/main/java/com/example/sempertibi/MainActivity.kt
@@ -1,44 +1,26 @@
 package com.example.sempertibi
 
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var timer: Timer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val enterButton = findViewById<Button>(R.id.btnStart)
-
-        enterButton.setOnClickListener{
-            val intent = Intent(this,SigninActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.my_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menuDashboard -> {
-                // Handle menu item 1 click
-                return true
+        timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@MainActivity, SigninActivity::class.java)
+                startActivity(intent)
+                finish()
             }
-            R.id.menuSignout -> {
-                // Handle menu item 2 click
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
-        }
+        }, 3500)
     }
 }
