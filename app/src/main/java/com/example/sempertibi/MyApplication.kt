c2050ef59ff10f08369100a7348aa0e480d8ebd3
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-03-25 17:07:56 +0100

    updated for notifications

--- app/src/main/java/com/example/sempertibi/MyApplication.kt
+++ app/src/main/java/com/example/sempertibi/MyApplication.kt
@@ -1,12 +1,36 @@
 package com.example.sempertibi
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 
 /*
 TODO check the Manifest implementation for this class, so app does not crash
  */
 class MyApplication : Application() {
 
+    // Notifications to the User as Reminder to use the App (As this app needs to be tested for the Master Thesis)
+    // Called when the application is starting, before any other application objects have been created.
+    override fun onCreate() {
+
+        super.onCreate()
+        // Create the NotificationChannel
+        val channelID = getString(R.string.channel_id)
+        val name = getString(R.string.channel_name)
+        val descriptionText = getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel =
+            NotificationChannel(channelID, name, importance).apply { description = descriptionText }
+
+        // Register the channel with the system
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+
+    // Global Data should be cleared when User terminates the App
     private fun clearGlobalData() {
         GlobalData.userID = null
         GlobalData.loggedInUser = null
@@ -16,10 +40,12 @@ class MyApplication : Application() {
         GlobalData.genderUser = null
         GlobalData.pssScore = 0
         GlobalData.hrvScore = 0
+        GlobalData.dateToday = null
     }
 
     override fun onTerminate() {
         super.onTerminate()
         clearGlobalData()
     }
+
 }
