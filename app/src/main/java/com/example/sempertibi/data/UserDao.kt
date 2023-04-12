441ac05ec97a9b6cabc8407d84cb0a0ae3822393
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-04-12 15:58:19 +0200

    Added a query

--- app/src/main/java/com/example/sempertibi/data/UserDao.kt
+++ app/src/main/java/com/example/sempertibi/data/UserDao.kt
@@ -44,6 +44,9 @@ interface UserDao {
     @Query("SELECT * FROM moodJournal WHERE user_id = :user_id AND entry_date = :entry_date")// Query the situation, that is inserted in the Moodjournal for a specific day
     fun readMoodOfUser(user_id: Int, entry_date: String): MoodJournal?
 
+    @Query("SELECT mood_score FROM moodJournal WHERE user_id = :user_id ORDER BY entry_date DESC LIMIT 7")
+    fun getMoodLast7Entries(user_id: Int): List<Int>
+
     @Update
     suspend fun updateMood(moodJournal: MoodJournal)
 
