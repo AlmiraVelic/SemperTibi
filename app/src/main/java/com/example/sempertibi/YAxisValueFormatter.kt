e31b9dbbf62ac45b347c13e60855396831bd6f57
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-04-12 15:58:36 +0200

    YAxis formatter adjusted

--- app/src/main/java/com/example/sempertibi/YAxisValueFormatter.kt
+++ app/src/main/java/com/example/sempertibi/YAxisValueFormatter.kt
@@ -5,10 +5,11 @@ import com.github.mikephil.charting.formatter.ValueFormatter
 
 class YAxisValueFormatter : ValueFormatter() {
     override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-        return when {
-            value <= 0f -> "Low"
-            value <= 25f -> "Medium"
-            else -> "High"
+        return when (value) {
+            0f -> "Low"
+            20f -> "Medium"
+            40f -> "High"
+            else -> ""
         }
     }
 }
