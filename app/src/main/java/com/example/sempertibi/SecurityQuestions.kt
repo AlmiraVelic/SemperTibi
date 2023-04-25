412711ba54a3340a6750c7a16ac826e380761686
AlmiraVelic <almira.velic@edu.fh-kaernten.ac.at>
2023-04-25 14:08:36 +0200

    Questions are inserted to the MySQL Database

--- app/src/main/java/com/example/sempertibi/SecurityQuestions.kt
+++ app/src/main/java/com/example/sempertibi/SecurityQuestions.kt
@@ -3,11 +3,14 @@ package com.example.sempertibi
 import android.content.Intent
 import android.os.Bundle
 import android.os.StrictMode
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.sempertibi.data.UserDatabase
 import com.example.sempertibi.data.entities.SecurityQuestion
 import com.google.android.material.textfield.TextInputEditText
@@ -15,9 +18,16 @@ import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONObject
 import uk.co.jakebreen.sendgridandroid.SendGrid
 import uk.co.jakebreen.sendgridandroid.SendGridMail
 import uk.co.jakebreen.sendgridandroid.SendTask
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 import java.util.*
 
 class SecurityQuestions : AppCompatActivity() {
@@ -45,13 +55,43 @@ class SecurityQuestions : AppCompatActivity() {
         initializeViews()
         timer = Timer()
 
+        val email = GlobalData.emailUser.toString()
         lifecycleScope.launch {
 
-            val userDB =  withContext(Dispatchers.IO) { dao.getUserByMail(GlobalData.emailUser.toString())}
+            val userDB = withContext(Dispatchers.IO) { dao.getUserByMail(email) }
             val userID = userDB!!.user_id
+
+            val userIDServer = getUserID(email)
+            Log.d("mysql", "userID set")
 
             saveButton.setOnClickListener {
                 if (validateQuestion1() and validateQuestion2() and validateQuestion3() and validateQuestion4()) {
+
+                    // server db
+                    insertSecurityQuestion(
+                        userIDServer,
+                        getString(R.string.security_question_1),
+                        streetNameInput.text.toString()
+                    )
+                    insertSecurityQuestion(
+                        userIDServer,
+                        getString(R.string.security_question_2),
+                        motherNameInput.text.toString()
+                    )
+                    insertSecurityQuestion(
+                        userIDServer,
+                        getString(R.string.security_question_3),
+                        movieNameInput.text.toString()
+                    )
+                    insertSecurityQuestion(
+                        userIDServer,
+                        getString(R.string.security_question_4),
+                        colorInput.text.toString()
+                    )
+
+                    Log.d("mysql", "questions inserted")
+
+                    // local db
                     val insertQuestions = listOf(
                         SecurityQuestion(
                             question_id = 0,
@@ -82,6 +122,12 @@ class SecurityQuestions : AppCompatActivity() {
                     lifecycleScope.launch {
                         insertQuestions.forEach { dao.insertSecurityQuestions(it) }
                     }
+
+                    Toast.makeText(
+                        applicationContext,
+                        "Security question inserted successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
 
                     emptyInputEditText()
 
@@ -212,4 +258,76 @@ class SecurityQuestions : AppCompatActivity() {
         val task = SendTask(sendGrid)
         task.send(mail)
     }
+
+    private fun getUserID(email: String): Int {
+        val url = URL("http://192.168.0.192/sempertibi/getuserIDbymail.php")
+
+        Log.d("mysql", "1")
+
+        val conn = url.openConnection() as HttpURLConnection
+        Log.d("mysql", "2")
+        conn.requestMethod = "POST"
+        Log.d("mysql", "3")
+        conn.doOutput = true
+        Log.d("mysql", "4")
+
+        val writer = OutputStreamWriter(conn.outputStream)
+        Log.d("mysql", "5")
+        writer.write("email=${URLEncoder.encode(email, "UTF-8")}")
+        Log.d("mysql", "6")
+        writer.flush()
+        Log.d("mysql", "7")
+
+        val reader = BufferedReader(InputStreamReader(conn.inputStream))
+        Log.d("mysql", "8")
+        val response = reader.readLine()
+        Log.d("mysql", "9")
+        reader.close()
+        Log.d("mysql", "10")
+
+        conn.disconnect()
+        Log.d("mysql", "11")
+
+        val jsonObject = JSONObject(response)
+        Log.d("mysql", "12")
+        if (jsonObject.has("error")) {
+            Log.d("mysql", "13")
+            throw RuntimeException(jsonObject.getString("error"))
+        } else {
+            Log.d("mysql", "14")
+            return jsonObject.getInt("user_id")
+        }
+    }
+
+    private fun insertSecurityQuestion(userId: Int, questionText: String, answer: String) {
+        val url = "http://192.168.0.192/sempertibi/insert_questions.php"
+        val request = object : StringRequest(
+            Method.POST,
+            url,
+            { _ ->
+                // Handle the response from the server
+            },
+            { error ->
+                // Handle the error
+                Toast.makeText(
+                    applicationContext,
+                    "Error: " + error.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        ) {
+            override fun getParams(): MutableMap<String, String> {
+                return mutableMapOf(
+                    "user_id" to userId.toString(),
+                    "question_text" to questionText,
+                    "answer" to answer
+                )
+            }
+        }
+
+        // Add the request to the request queue
+        Volley.newRequestQueue(this@SecurityQuestions).add(request)
+    }
+
+
 }
